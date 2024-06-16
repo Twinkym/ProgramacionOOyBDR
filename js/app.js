4a381c90f1d45e1ae2d65886babd58ae2e32e18a
David De La Puente Enriquez <Twinkymen@hotmail.com>
2024-06-16 14:01:32 +0200

    app.js rutes components changed

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-import HeaderComponent from 'https://github.com/Twinkym/TAREAS/blob/TareasconAntonio/TAREAS_CON_RITA/GITHUB/DavidP_gri/ProgramacionOOyBDR/js/components/HeaderComponent.js';
+import HeaderComponent from './components/HeaderComponent';
 import CourseComponent from './components/CourseComponent';
 
 document.getElementById('app').innerHTML = `${HeaderComponent()} ${CourseComponent()}`;
